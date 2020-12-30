5110cb29a497ca47046481cad8a31b0d5d5fbdaf
0xLeif <zmeriksen@icloud.com>
2020-12-29 18:19:22 -0600

    Basic SDButton action

--- Sources/ScreenDataUI/Views/SDButton.swift
+++ Sources/ScreenDataUI/Views/SDButton.swift
@@ -8,7 +8,17 @@
 import SwiftUI
 import ScreenData
 
+public typealias SDAction = () -> Void
+
 public struct SDButton: View {
+    public static var actions: [String: SDAction] = [:]
+    public static func add(
+        actionWithID actionID: String,
+        action: @escaping SDAction
+    ) {
+        actions[actionID] = action
+    }
+    
     public var button: SomeButton
     
     public init(button: SomeButton) {
@@ -18,6 +28,14 @@ public struct SDButton: View {
     public var body: some View {
         Button(action: {
             print("[SDButton] ActionID: \(String(describing: button.actionID)) & Destination: \(String(describing: button.destination))")
+            if let actionID = button.actionID,
+               let action = SDButton.actions[actionID] {
+                action()
+            }
+            
+            if let destination = button.destination {
+                print("TODO: Navigate to destination")
+            }
         }) {
             Text(button.title)
         }
