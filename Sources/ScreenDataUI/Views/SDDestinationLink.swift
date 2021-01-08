475c06f8c426761fa9f9a1e01aeb014a0d61ff75
0xLeif <zmeriksen@icloud.com>
2021-01-07 19:23:02 -0600

    Added DestinationStore for SDDestinationLink

--- Sources/ScreenDataUI/Views/SDDestinationLink.swift
+++ Sources/ScreenDataUI/Views/SDDestinationLink.swift
@@ -10,9 +10,28 @@ import Combine
 import ScreenData
 import ScreenDataNavigation
 
+public class SDDestinationStore: ObservableObject {
+    @Published public var destinationView: SDScreen?
+    
+    private var task: AnyCancellable?
+    
+    public func load(destination: Destination?, provider: ScreenProviding) {
+        guard let destination = destination else {
+            return
+        }
+        
+        task = provider.screen(forID: destination.toID)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in },
+                  receiveValue: { (screen) in
+                    self.destinationView = SDScreen(screen: screen)
+                  })
+    }
+}
+
 public struct SDDestinationLink<Content>: View where Content: View {
-    @State private var task: AnyCancellable?
-    @State private var destinationView: SDScreen?
+    @StateObject private var store: SDDestinationStore = SDDestinationStore()
+    
     public var provider: ScreenProviding
     
     public var destination: Destination?
@@ -33,7 +52,7 @@ public struct SDDestinationLink<Content>: View where Content: View {
             return AnyView(content())
         }
         
-        guard let destinationView = destinationView else {
+        guard let destinationView = store.destinationView else {
             return AnyView(loadingView)
         }
         
@@ -52,12 +71,8 @@ public struct SDDestinationLink<Content>: View where Content: View {
         
         return AnyView(ProgressView()
                         .onAppear {
-                            task = provider.screen(forID: destination.toID)
-                                .receive(on: DispatchQueue.main)
-                                .sink(receiveCompletion: { _ in },
-                                      receiveValue: { (screen) in
-                                        self.destinationView = SDScreen(screen: screen)
-                                      })
+                            store.load(destination: destination,
+                                       provider: provider)
                         })
     }
 }
