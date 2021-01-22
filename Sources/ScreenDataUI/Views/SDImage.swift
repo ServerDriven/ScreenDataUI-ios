7246a243db5150483e095af39f772632825e8fa9
0xLeif <zmeriksen@icloud.com>
2021-01-21 19:38:20 -0600

    Fixed tinting of ProgressView

--- Sources/ScreenDataUI/Views/SDImage.swift
+++ Sources/ScreenDataUI/Views/SDImage.swift
@@ -12,18 +12,27 @@ public struct SDImage: View {
     public static var defaultForegroundColor: Color?
     
     public var image: SomeImage
+    private var progressTint: Color
     
     public init(image: SomeImage) {
         self.image = image
+        
+        if let style = image.style,
+           let foregroundColor = style.foregroundColor,
+           let tint = SDColor(color: foregroundColor).body as? Color {
+            self.progressTint = tint
+        } else if let defaultTint = SDImage.defaultForegroundColor {
+            self.progressTint = defaultTint
+        } else {
+            self.progressTint = .primary
+        }
     }
     
     public var body: some View {
         SDDestinationLink(provider: SDScreenProvider(), destination: image.destination) {
             AsyncImage(url: URL(string: image.url)!) {
                 ProgressView()
-                    .foregroundColor(image.style?.foregroundColor.map {
-                        SDColor(color: $0).body as? Color
-                    } ?? SDImage.defaultForegroundColor ?? .primary)
+                    .progressViewStyle(CircularProgressViewStyle(tint: progressTint))
             }
             .aspectRatio(contentMode: .fit)
             .background(with: image.style)
