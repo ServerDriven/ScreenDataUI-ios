8632d93684ab991d062df9bd4c33b9039ebbf093
Zach Eriksen <zmeriksen@icloud.com>
2021-05-13 16:07:56 -0500

    Update packages

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "ScreenData", url: "https://github.com/ServerDriven/ScreenData-swift", from: "0.4.1"),
-        .package(name: "ScreenDataNavigation", url: "https://github.com/ServerDriven/ScreenDataNavigation-swift", from: "0.4.1")
+        .package(name: "ScreenDataNavigation", url: "https://github.com/ServerDriven/ScreenDataNavigation-swift", .branch("develop")),
+        .package(url: "https://github.com/0xLeif/FlatMany", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +28,8 @@ let package = Package(
             name: "ScreenDataUI",
             dependencies: [
                 "ScreenData",
-                "ScreenDataNavigation"
+                "ScreenDataNavigation",
+                "FlatMany"
             ]),
         .testTarget(
             name: "ScreenDataUITests",
