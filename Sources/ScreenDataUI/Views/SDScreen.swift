9e801e9aa12fc4338c6959232d48928b69276f9c
Zach Eriksen <zmeriksen@icloud.com>
2021-05-25 17:57:21 -0500

    Added frame with screen height and width

--- Sources/ScreenDataUI/Views/SDScreen.swift
+++ Sources/ScreenDataUI/Views/SDScreen.swift
@@ -51,5 +51,6 @@ public struct SDScreen: View, Equatable {
                           blue: Double(screen.backgroundColor.blue),
                           opacity: Double(screen.backgroundColor.alpha)))
         .navigationBarTitle(screen.title)
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
     }
 }
