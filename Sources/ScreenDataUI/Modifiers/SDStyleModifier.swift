6526246143ca60df4fdf29db0bc1ce9be8bf98df
Zach Eriksen <zeriksen@fnoex.com>
2021-07-13 13:48:09 -0500

    Added ViewBuilder to SDStyleModifier

--- Sources/ScreenDataUI/Modifiers/SDStyleModifier.swift
+++ Sources/ScreenDataUI/Modifiers/SDStyleModifier.swift
@@ -27,51 +27,45 @@ public struct SDStyleModifier: ViewModifier {
         return CGFloat(height)
     }
     
+    @ViewBuilder
     public func body(content: Content) -> some View {
         if style.isHidden {
-            return AnyView(
-                content
-                    .hidden()
-                    .frame(width: 0, height: 0, alignment: .center)
-            )
+            content
+                .hidden()
+                .frame(width: 0, height: 0, alignment: .center)
         } else if style.padding != 0 {
-            return AnyView(
-                content
-                    .padding(CGFloat(style.padding))
-                    .background(
-                        style.backgroundColor.map {
-                            AnyView(SDColor(color: $0))
-                        } ?? AnyView(Color.clear)
-                    )
-                    .cornerRadius(CGFloat(style.cornerRadius))
-                    .foregroundColor(style.foregroundColor.map {
-                        SDColor(color: $0).body as? Color
-                    } ?? Color.primary)
-                    .frame(
-                        width: width,
-                        height: height,
-                        alignment: .center
-                    )
-            )
+            content
+                .padding(CGFloat(style.padding))
+                .background(
+                    style.backgroundColor.map {
+                        AnyView(SDColor(color: $0))
+                    } ?? AnyView(Color.clear)
+                )
+                .cornerRadius(CGFloat(style.cornerRadius))
+                .foregroundColor(style.foregroundColor.map {
+                    SDColor(color: $0).body as? Color
+                } ?? Color.primary)
+                .frame(
+                    width: width,
+                    height: height,
+                    alignment: .center
+                )
         } else {
-            return AnyView(
-                content
-                    .background(
-                        style.backgroundColor.map {
-                            AnyView(SDColor(color: $0))
-                        } ?? AnyView(Color.clear)
-                    )
-                    .cornerRadius(CGFloat(style.cornerRadius))
-                    .foregroundColor(style.foregroundColor.map {
-                        SDColor(color: $0).body as? Color
-                    } ?? Color.primary)
-                    .frame(
-                        width: width,
-                        height: height,
-                        alignment: .center
-                    )
-            )
+            content
+                .background(
+                    style.backgroundColor.map {
+                        AnyView(SDColor(color: $0))
+                    } ?? AnyView(Color.clear)
+                )
+                .cornerRadius(CGFloat(style.cornerRadius))
+                .foregroundColor(style.foregroundColor.map {
+                    SDColor(color: $0).body as? Color
+                } ?? Color.primary)
+                .frame(
+                    width: width,
+                    height: height,
+                    alignment: .center
+                )
         }
-        
     }
 }
