4e4c6b8184faeb20cd82abf9c4577c09ba631862
Zach Eriksen <zmeriksen@icloud.com>
2022-03-05 14:06:35 -0700

    Remove UserDefault Image storing for File storing

--- Sources/ScreenDataUI/Helpers/SDImage/SDImageStoring.swift
+++ Sources/ScreenDataUI/Helpers/SDImage/SDImageStoring.swift
@@ -13,16 +13,21 @@ public protocol SDImageStoring {
     func store(image: UIImage?, forURL url: URL) -> AnyPublisher<Void, Error>
 }
 
-public struct SDImageUserDefaultsStorer: SDImageStoring {
+public struct SDImageFileStore: SDImageStoring {
     public init() { }
     
     public func store(image: UIImage?, forURL url: URL) -> AnyPublisher<Void, Error> {
         Future { promise in
+            let key = url.absoluteString.replacingOccurrences(of: "/", with: "-")
+            let path = FileManager.default.urls(
+                for: .documentDirectory,
+                   in: .userDomainMask
+            )[0].appendingPathComponent(key)
+            
             if let imageData = image?.pngData() {
-                UserDefaults.standard.set(imageData, forKey: url.absoluteString)
-            } else {
-                UserDefaults.standard.set(nil, forKey: url.absoluteString)
+                try? imageData.write(to: path)
             }
+            
             promise(.success(()))
         }
         .eraseToAnyPublisher()
