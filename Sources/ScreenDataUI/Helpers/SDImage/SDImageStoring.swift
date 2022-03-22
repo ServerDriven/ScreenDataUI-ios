56f0f98a41e248711aaf30a2d4dd6343fa5fb4c2
Zach Eriksen <zmeriksen@icloud.com>
2022-03-21 19:42:59 -0600

    Update all URL and File tasks to FLet

--- Sources/ScreenDataUI/Helpers/SDImage/SDImageStoring.swift
+++ Sources/ScreenDataUI/Helpers/SDImage/SDImageStoring.swift
@@ -5,9 +5,10 @@
 //  Created by Leif on 5/19/21.
 //
 
+import Combine
+import FLet
 import Foundation
 import SwiftUI
-import Combine
 
 public protocol SDImageStoring {
     func store(image: UIImage?, forURL url: URL) -> AnyPublisher<Void, Error>
@@ -19,13 +20,9 @@ public struct SDImageFileStore: SDImageStoring {
     public func store(image: UIImage?, forURL url: URL) -> AnyPublisher<Void, Error> {
         Future { promise in
             let key = url.absoluteString.replacingOccurrences(of: "/", with: "-")
-            let path = FileManager.default.urls(
-                for: .documentDirectory,
-                   in: .userDomainMask
-            )[0].appendingPathComponent(key)
             
             if let imageData = image?.pngData() {
-                try? imageData.write(to: path)
+                try? __.transput.file.out(imageData, filename: key)
             }
             
             promise(.success(()))
